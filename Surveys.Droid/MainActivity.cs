1e71741972de4432be0015d7da70568009fc68de
Adrián Romero <rgfxadrian@outlook.com>
2017-08-06 23:09:42 -0500

    Agregados proyectos, referenciando Xamarin Forms, falta UWP

---
+++ Surveys.Droid/MainActivity.cs
@@ -0,0 +1,22 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Xamarin.Forms.Platform.Android;
+
+namespace Surveys.Droid
+{
+    [Activity(Label = "Surveys.Droid", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : FormsApplicationActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            Xamarin.Forms.Forms.Init(this, bundle);
+            LoadApplication(new Surveys.Core.App());
+            // Set our view from the "main" layout resource
+            // SetContentView (Resource.Layout.Main);
+        }
+    }
+}
+
