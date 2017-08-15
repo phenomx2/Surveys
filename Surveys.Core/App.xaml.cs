99236d75c8c3ee4c11eaced6a8989112916b09be
Adrián Romero <rgfxadrian@outlook.com>
2017-08-14 20:58:07 -0500

    Implementando modelo de aplicacion PRISM

--- Surveys.Core/App.xaml.cs
+++ Surveys.Core/App.xaml.cs
@@ -3,19 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Prism.Unity;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using Surveys.Core.Views;
+
 namespace Surveys.Core
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class App : Application
+    public partial class App : PrismApplication
     {
         public App()
         {
+            
+            //MainPage = new NavigationPage(new Views.Surveys());
+        }
+
+        protected override async void OnInitialized()
+        {
             InitializeComponent();
-            MainPage = new NavigationPage(new Views.Surveys());
+            await NavigationService.NavigateAsync($"{nameof(RootNavigation)}/{nameof(Views.Surveys)}");
+        }
+
+        protected override void RegisterTypes()
+        {
+            Container.RegisterTypeForNavigation < Views.RootNavigation>();
+            Container.RegisterTypeForNavigation<Views.Surveys>();
+            Container.RegisterTypeForNavigation<Views.SurveyDetail>();
         }
     }
 }
---
+++ Surveys.Core/Views/RootNavigation.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Surveys.Core.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class RootNavigation : NavigationPage
+	{
+		public RootNavigation ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
