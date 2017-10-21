0fdfbdec6470c6466d48141aa343dc9bde2594d4
Adrián Romero <rgfxadrian@outlook.com>
2017-10-21 15:39:16 -0500

    Añadiendo autenticacion basada en tokens

---
+++ Surveys.Web/Providers/AppOAuthProvider.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.OAuth;
+
+namespace Surveys.Web.Providers
+{
+    public class AppOAuthProvider : OAuthAuthorizationServerProvider
+    {
+        private readonly string _publicClientId;
+
+        public AppOAuthProvider(string publicClientId)
+        {
+            if (publicClientId == null)
+                throw new ArgumentNullException(nameof(publicClientId), "El identificador no es válido");
+
+            _publicClientId = publicClientId;
+        }
+
+        public override Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
+        {
+            context.Response.Headers.Add("Access-Control-Allow-Origin",new []{ "*" });
+
+            //Aquí va la logica real
+            if (context.UserName == "libro" && context.Password == "xamarin")
+            {
+                //Crea y prepara el objeto ClaimsIdentity
+                var identity = new ClaimsIdentity(OAuthDefaults.AuthenticationType);
+                identity.AddClaim(new Claim(ClaimTypes.Name, "Adrián Romero"));
+                var data = new Dictionary<string, string>
+                {
+                    {"email", "rgfxadrian@outlook.com"}
+                };
+                var properties = new AuthenticationProperties(data);
+                //Crea un AutheticationTicket con la identidad y las propiedades
+                var ticket = new AuthenticationTicket(identity,properties);
+                //Valida y autentica
+                context.Validated(ticket);
+                return Task.FromResult(true);
+            }
+            else
+            {
+                context.SetError("access_denied","Acceso denegado");
+                return Task.FromResult(false);
+            }
+        }
+
+        public override async Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
+        {
+            await Task.Run(() => context.Validated());
+        }
+
+        public override Task TokenEndpoint(OAuthTokenEndpointContext context)
+        {
+            foreach (KeyValuePair<string, string> property in context.Properties.Dictionary)
+            {
+                context.AdditionalResponseParameters.Add(property.Key,property.Value);
+            }
+            return Task.FromResult<object>(null);
+        }
+    }
+}
---
+++ Surveys.Web/Startup.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Http;
+using Owin;
+
+namespace Surveys.Web
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var config = new HttpConfiguration();
+            OAuthConfig.ConfigureOAuth(app,config);
+
+            app.UseWebApi(config);
+            WebApiConfig.Register(config);
+        }
+    }
+}
